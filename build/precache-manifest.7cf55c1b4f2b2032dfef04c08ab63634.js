c2f23b7ce102a729c13dfb25bee2e08e7ffe5b12
khuonghuy <huy.khuong.000@gmail.com>
2023-04-02 15:49:32 +0700

    second

---
+++ build/precache-manifest.7cf55c1b4f2b2032dfef04c08ab63634.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80d0b9fb92eea8098d7d121d1531b2f",
+    "url": "/trankhuonghuy1997/ass2-react/index.html"
+  },
+  {
+    "revision": "e92b48efcd70af14f6a1",
+    "url": "/trankhuonghuy1997/ass2-react/static/css/main.7b23c558.chunk.css"
+  },
+  {
+    "revision": "e93cc4e98aa1ceef474f",
+    "url": "/trankhuonghuy1997/ass2-react/static/js/2.60899f06.chunk.js"
+  },
+  {
+    "revision": "c1f0f4da96b8067f4f0dfc4881d93bf3",
+    "url": "/trankhuonghuy1997/ass2-react/static/js/2.60899f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e92b48efcd70af14f6a1",
+    "url": "/trankhuonghuy1997/ass2-react/static/js/main.c135f0aa.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/trankhuonghuy1997/ass2-react/static/js/main.c135f0aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ccb14d404f93aee42e",
+    "url": "/trankhuonghuy1997/ass2-react/static/js/runtime-main.9a29a1ea.js"
+  }
+]);
